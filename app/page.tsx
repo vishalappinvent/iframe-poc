e28f0da1edbe65f96340a3ce85085e9089e7ef7f
Vishal Singh <vishal.singh@appinventiv.com>
2024-08-22 15:43:47 +0530

    init

--- app/page.tsx
+++ app/page.tsx
@@ -3,14 +3,16 @@ import Image from "next/image";
 import { useEffect, useRef } from "react";
 
 export default function Home() {
-  const frame=useRef(null);
+  const frame = useRef < HTMLIFrameElement>(null);
   const changeColor=()=>{
     const hex =
       "#" + ((Math.random() * 0xffffff) << 0).toString(16).padStart(6, "0");
-    frame.current.contentWindow.postMessage(
-      { color: hex },
-      "https://iframe-theme.vercel.app/"
-    );
+    if (frame.current && frame.current.contentWindow) {
+      frame.current.contentWindow.postMessage(
+        { color: hex },
+        "https://iframe-theme.vercel.app/"
+      );
+    }
   }
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
