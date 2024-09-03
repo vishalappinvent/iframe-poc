d390aac4e6bc74c8c70e4f5b081dcc54e6801a2c
Pratik Jha <pratik.jha@appinventiv.com>
2024-09-03 12:54:40 +0530

    functionality of background color capture and data transfer-iframe to parent

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,16 @@
 "use client"
 import Image from "next/image";
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 
 export default function Home() {
+  const [capturedColour,setColour]=useState('')
+  useEffect(()=>{
+    window.addEventListener("message", (event)=>{
+      const data=JSON.parse(event.data)
+      console.log('event from iframe',data);
+      setColour(data?.backgroundColor)
+     });
+  },[])
   const frame = useRef < HTMLIFrameElement>(null);
   const changeColor=()=>{
     const hex =
@@ -10,14 +18,15 @@ export default function Home() {
     if (frame.current && frame.current.contentWindow) {
       frame.current.contentWindow.postMessage(
         { color: hex },
-        "https://iframe-theme.vercel.app/"
+        "http://localhost:3001"
       );
     }
   }
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <iframe ref={frame} src="https://iframe-theme.vercel.app"></iframe>
+      <iframe ref={frame} src="http://localhost:3001"></iframe>
       <button onClick={changeColor}>Change Color</button>
+      <>Captured colour is {capturedColour}</>
     </main>
   );
 }
